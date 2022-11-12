2bef2e493f8d097764fcde639c7cbb5b1fdd5f17
Jaya Kesarwani <48471209+meik235@users.noreply.github.com>
2022-11-12 19:28:32 +0530

    Delete AssignmentTwo directory

--- AssignmentTwo/ConverterApp/main.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("kgsInput").addEventListener("input", (event) => {
-  let kgs = event.target.value;
-  document.getElementById("lbsOutput").innerText = kgs * 2.205;
-});
-
-document.getElementById("feetInput").addEventListener("input", (event) => {
-  let feet = event.target.value;
-  document.getElementById("cmsOutput").innerText = feet * 30.48;
-});
